712175398d8f3bdf9d9af6483c71bc449e2d77e6
Brian Bugert <coolbots7@gmail.com>
2020-06-26 17:48:30 -0700

    Update OH url. Add OH url env variable.

--- src/backend/server.js
+++ src/backend/server.js
@@ -4,18 +4,16 @@ const bodyParser = require('body-parser');
 const axios = require('axios');
 
 const PORT = process.env.PORT || 3000;
+const OPENHAB_URL = process.env.OPENHAB_URL || "http://openhab.cb7.com";
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const OPENHAB_URL = "http://openhab.cb7.com:8080/rest/";
-
 const getOpenHabItem = async (item) => {
-    const url = `${OPENHAB_URL}items/${item}`;
-
+    const url = `${OPENHAB_URL}/rest/items/${item}`;
     return await axios.get(url);
-}
+};
 
 app.get('/', function (req, res) {
     return res.send('Hello world');
